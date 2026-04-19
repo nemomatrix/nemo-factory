46c974e4ed6cf38b62677575fab7582e4cd2a6ae
nemomatrix <mustafa85kassab@gmail.com>
2026-04-19 23:45:53 +0300

    Update task.rs

--- crates/core/src/task.rs
+++ crates/core/src/task.rs
@@ -1,1 +1,20 @@
+use uuid::Uuid;
 
+#[derive(Debug, Clone)]
+pub struct Task {
+    pub id: Uuid,
+    pub name: String,
+    pub dependencies: Vec<Uuid>,
+    pub completed: bool,
+}
+
+impl Task {
+    pub fn new(name: &str, dependencies: Vec<Uuid>) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name: name.to_string(),
+            dependencies,
+            completed: false,
+        }
+    }
+}
