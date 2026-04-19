c2fa72a4780f3f8820856519e333a05a3569b7df
nemomatrix <mustafa85kassab@gmail.com>
2026-04-20 00:18:26 +0300

    Update patch_builder.rs

--- crates/repair_engine/src/patch_builder.rs
+++ crates/repair_engine/src/patch_builder.rs
@@ -1,1 +1,9 @@
+use crate::log_analyzer::parser::ParsedError;
 
+pub fn build_patch(error: ParsedError) -> String {
+    match error.error_type.as_str() {
+        "RUST_BORROW" => "// fix borrow issue".into(),
+        "MISSING_DEP" => "// add missing dependency".into(),
+        _ => "// generic fix".into(),
+    }
+}
