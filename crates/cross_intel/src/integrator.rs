cec65f3edb634009ad8310b8829f679faa50a6d6
nemomatrix <mustafa85kassab@gmail.com>
2026-04-20 17:30:22 +0300

    Update integrator.rs

--- crates/cross_intel/src/integrator.rs
+++ crates/cross_intel/src/integrator.rs
@@ -1,1 +1,16 @@
+use crate::adapter::Adapter;
+use crate::validator::Validator;
+use crate::capability::Capability;
 
+pub struct Integrator;
+
+impl Integrator {
+    pub fn integrate(caps: Vec<Capability>) {
+        for c in caps {
+            if Validator::validate(&c) {
+                let module = Adapter::adapt(&c);
+                println!("INTEGRATED: {}", module);
+            }
+        }
+    }
+}
