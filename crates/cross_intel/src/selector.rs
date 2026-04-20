7a6f0edc409f90a639c6244afd2f9f4c936e1336
nemomatrix <mustafa85kassab@gmail.com>
2026-04-20 17:27:09 +0300

    Update selector.rs

--- crates/cross_intel/src/selector.rs
+++ crates/cross_intel/src/selector.rs
@@ -1,1 +1,10 @@
+use crate::capability::Capability;
 
+pub struct Selector;
+
+impl Selector {
+    pub fn select(mut caps: Vec<Capability>) -> Vec<Capability> {
+        caps.sort_by(|a, b| b.strength.partial_cmp(&a.strength).unwrap());
+        caps.into_iter().take(3).collect()
+    }
+}
